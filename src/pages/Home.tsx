94df34c3c6ef156b9b62336e2fa08b9c3063080c
igorfiquene <igorfpessoa@gmail.com>
2022-01-30 19:56:58 -0300

    feat: handle exists room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,12 +8,15 @@ import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
 import { useAuth } from '../hooks/useAuth';
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
 
 
 
 export function Home() {
 	const history = useHistory();
 	const { user, signInWithGoogle } = useAuth()
+	const [roomCode, setRoomCode] = useState('')
 
 	async function handleCreateRoom() {
 		if (!user) {
@@ -21,6 +24,23 @@ export function Home() {
 		}
 
 		history.push('/rooms/new')
+	}
+
+	async function handleJoinRoom(event: FormEvent) {
+		event.preventDefault()
+
+		if (roomCode.trim() === '') {
+			return
+		}
+
+		const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+		if (!roomRef.exists()) {
+			alert('Rooms does not exists.')
+			return;
+		}
+
+		history.push(`/rooms/${roomCode}`)
 	}
 
 	return (
@@ -39,10 +59,12 @@ export function Home() {
 					</button>
 					<div className="separator">ou entre em uma sala</div>
 
-					<form>
+					<form onSubmit={handleJoinRoom}>
 						<input 
 							type="text"
 							placeholder="Digite o código da sala"
+							onChange={event => setRoomCode(event.target.value)}
+							value={roomCode}
 						/>
 						
 						<Button>
