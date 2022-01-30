75f34568c8f52d1c4677f673ed97eee277533c8c
igorfiquene <igorfpessoa@gmail.com>
2022-01-30 19:06:29 -0300

    feat: redirect to new room

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,5 +1,5 @@
 import { FormEvent, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
@@ -10,7 +10,7 @@ import { database } from '../services/firebase';
 
 export function NewRoom() {
 	const { user } = useAuth()
-
+	const history = useHistory()
 	const [newRoom, setNewRoom] = useState('')
 
 	async function handleCreateRoom(event: FormEvent) {
@@ -26,6 +26,8 @@ export function NewRoom() {
 			title: newRoom,
 			authorId: user?.id
 		})
+
+		history.push(`/rooms/${firebaseRoom.key}`)
 		
 	}
 	
